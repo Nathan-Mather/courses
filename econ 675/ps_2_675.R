c481c1326c33bc61464f5c7f487091611ea600ea
Nathan-Mather <njmather@umich.edu>
2018-10-12 00:51:51 -0400

    Fucked up code trying to make faster.
    Tried to use sapply and it went way way slower.

--- econ 675/ps_2_675.R
+++ econ 675/ps_2_675.R
@@ -13,6 +13,7 @@ library(doParallel)
 library(foreach)
 library(ggplot2)
 library(Matrix)
+library(xtable)
 
 # clear data and consol 
 rm(list = ls(pos = ".GlobalEnv"), pos = ".GlobalEnv")
@@ -20,7 +21,17 @@ options(scipen = 999)
 cat("\f")
 
 # set options 
-opt_test_run <- FALSE
+opt_test_run <- TRUE
+
+# set attributes for plot to default ea theme 
+plot_attributes <- theme( plot.background = element_rect(fill = "lightgrey"),
+                          panel.grid.major.x = element_line(color = "gray90"), 
+                          panel.grid.minor  = element_blank(),
+                          panel.background = element_rect(fill = "white", colour = "black") , 
+                          panel.grid.major.y = element_line(color = "gray90"),
+                          text = element_text(size= 20),
+                          plot.title = element_text(vjust=0, colour = "#0B6357",face = "bold", size = 40))
+
 
 #================================================#
 # ==== Question 1: Kernel Density Estimation ====
@@ -83,31 +94,20 @@ h_opt <- (15/(v2k*1000))^(1/5)
     # in_data <- r_dt
     # x_v <- "rdraw"
 
-    # # generate data for debugging functions
-    # # start data.table for random data, take a random draw for weighted normals
-    # r_dt <- data.table( r1 = sample(1:2,prob=c(.5,.5),size=n,replace=T) )
-    # 
-    # # draw a random number from appropriate normal dist according to r1
-    # r_dt[r1 == 1, rdraw := rnorm(.N,-1.5,1.5)]
-    # r_dt[r1 == 2, rdraw := rnorm(.N,1,1)]
-    # r_dt[, r1 := NULL]
-    # in_data <- r_dt
-    # h_v <- c(.5,.6)
+    # generate data for debugging functions
+    # start data.table for random data, take a random draw for weighted normals
+    r_dt <- data.table( r1 = sample(1:2,prob=c(.5,.5),size=n,replace=T) )
+
+    # draw a random number from appropriate normal dist according to r1
+    r_dt[r1 == 1, rdraw := rnorm(.N,-1.5,1.5)]
+    r_dt[r1 == 2, rdraw := rnorm(.N,1,1)]
+    r_dt[, r1 := NULL]
+    in_data <- r_dt
+    h_v <- c(.5,.6)
+    x_v <- "rdraw"
 
     imse_f <- function(in_data, x_v, h_v = NULL, f_x = f_x){
-    
-      # copy the data to aviod editing it in global enviorment 
-      data <- copy(in_data)
       
-      # add a constant for the merge 
-      in_data[, const := 1]
-      
-      # cartesian merge to get all pairs 
-      paired_dt <- merge(in_data, in_data, by = "const", allow.cartesian = TRUE)
-      
-      # get new variable names after the merge. This kind of annoyingly general for a HW assingment. I regret nothing...
-      x_vx <- paste0(x_v, ".x")
-      x_vxi <- paste0(x_v, ".y")
       
       # initialize a list for output from each h 
       ouput_list <- vector("list", length= length(h_v))
@@ -117,35 +117,32 @@ h_opt <- (15/(v2k*1000))^(1/5)
         
         h <- h_v[[i]]
         
-        # get the kernal thing for each pair 
-        paired_dt[, k_x := K0((get(x_vxi) - get(x_vx))/h)]
-        
-        # now mean the kernal by rdraw.x and devide by h 
-        f_hats <- paired_dt[, list(f_hat_x = mean(k_x)/h), by = x_vx]
-        
-        # now get the f_hats for the leave one out by deleating the observation where x= xi. This will be rows 
-        # 1, M+2, 2M+3, 3M+4 ... so eq(1, M*M, M+1) should take care of those 
-        paired_dt_lo <- paired_dt[-c(seq(1, M*M, M+1)), ]
-        
-        # now get the mean of the f_hats leacing out the x 
-        f_hats_lo <- paired_dt_lo[, list(f_hat_x = mean(k_x)/h), by = x_vx]
-        
+        # Compute imse 
+         fhat_li <- sapply(as.matrix(in_data),function(x) 1/(n*h)*sum(K0((as.matrix(in_data)-x)/h))) 
+       
+       # Compute imse 
+         fhat_lo <- sapply(1:n,function(i) 1/(n*h)*sum(K0((as.matrix(in_data)[-i]-as.matrix(in_data)[i])/h))) 
+       
+        # put into data.table 
+         in_data[,fhat_li := fhat_li ]
+         in_data[,fhat_lo := fhat_lo ]
+  
         # now add in f_x for each 
-        f_hats[, f_x := f_x(get(x_vx))]
-        f_hats_lo[, f_x := f_x(get(x_vx))]
+        in_data[, f_x_li := f_x(get(x_v))]
+        in_data[, f_x_lo := f_x(get(x_v))]
         
         # now do squared error 
-        f_hats[, sq_er := (f_hat_x - f_x)^2]
-        f_hats_lo[, sq_er := (f_hat_x - f_x)^2]
+        in_data[, sq_er_li := (fhat_li - f_x_li)^2]
+        in_data[, sq_er_lo := (fhat_lo - f_x_lo)^2]
         
         # now get imse 
-        imse_li <- f_hats[, mean(sq_er)]
-        imse_lo <- f_hats_lo[, mean(sq_er)]
+        imse_li <- in_data[, mean(sq_er_li)]
+        imse_lo <- in_data[, mean(sq_er_lo)]
         
         # now put into a data.table and put in list 
         ouput_list[[i]] <- data.table(imse_li = imse_li, imse_lo= imse_lo, h = h)
       }
-        
+    
       output <- rbindlist(ouput_list)
   
       return(output[])
@@ -210,9 +207,9 @@ h_opt <- (15/(v2k*1000))^(1/5)
 
   # make a vector of h's 
   h_v <- seq(.5, 1.5,.1)
-  # lets time this sucker
+ 
+   # lets time this sucker
   start_t <- Sys.time()
-  
 
   # parallel setup
   cl <- makeCluster(4, type = "PSOCK")
@@ -227,28 +224,29 @@ h_opt <- (15/(v2k*1000))^(1/5)
   # stop clusters 
   stopCluster(cl)
   
-  
   # AND TIME 
-  run_time <- Sys.time() - start_t
+  run_time1 <- Sys.time() - start_t
   
   # bind list
   output_dt <- rbindlist(output_list)
   
   # now take the mean of imse 
-  imse_h <- output_dt[, list(imse_li = mean(imse_li), imse_lo = mean(imse_lo), d_h_hat = mean(d_h_hat)) ]
-  
-  # add in h 
-  imse_h[, h := h]
-  
-  h_list[[(h*10-4)]] <- imse_h
-  
-
-  # bind results 
-  part_b_res <- rbindlist(h_list)
+  part_b_res <- output_dt[, list(imse_li = mean(imse_li), imse_lo = mean(imse_lo), d_h_hat = mean(d_h_hat)), by = h ]
   
   # make them pretty 
   part_b_res_pretty <- signif(part_b_res, 3)
   part_b_res_pretty[, colnames(part_b_res_pretty)] <- lapply(part_b_res_pretty[,colnames(part_b_res_pretty), with = FALSE],  as.character)
+  
+  # make the graph 
+  # melt the data to work better with ggplot 
+  part_b_res[, d_h_hat := NULL ]
+  plot_data <-  melt.data.table(part_b_res, id.vars = "h", variable.name = "Error Type")
+  plot_1_3_b <- ggplot(data = plot_data, aes(x = h, y = value, color = `Error Type`, shape = `Error Type`, linetype = `Error Type`))
+  
+  plot_1_3_b <- plot_1_3_b + geom_point() + geom_line() + plot_attributes + xlim(c(.5,1.3)) + ylim(c(.000125, .0002))
+  plot_1_3_b
+  
+
   
   
 #====================#
@@ -264,9 +262,14 @@ h_opt <- (15/(v2k*1000))^(1/5)
           include.rownames = FALSE,
           floating = FALSE)
     
+    # save the plot 
+    png("c:/Users/Nmath_000/Documents/Code/courses/econ 675/PS_2_tex/plot_1_3_b.png", height = 800, width = 800, type = "cairo")
+    print(plot_1_3_b)
+    dev.off()
     
   }
   
+
 
   #=====================#
   # ==== Question 2 ====
@@ -371,35 +374,17 @@ h_opt <- (15/(v2k*1000))^(1/5)
   # check time 
   run_time <- Sys.time() - start_t
   
-  #  run without parallel 
-  # # now lapply the funciton 
-  # all_out <- lapply(c(1:100), power_s_fun)
-  # 
-  # # now merge all results  
-  # all_out_dt <-Reduce(function(x, y) merge(x, y, by = "k"), all_out)
-  # 
-  # run_time <- Sys.time() - start_t
-  
   # row sum my data to get the averaage cv for each k 
   all_out_dt[, k := NULL]
   mean_cv <- data.table( cv_means = rowMeans(all_out_dt), k = 1:20)
   
   # now plot that bad boy 
-  # set attributes for plot to default ea theme 
-  plot_attributes <- theme( plot.background = element_rect(fill = "lightgrey"),
-                            panel.grid.major.x = element_line(color = "gray90"), 
-                            panel.grid.minor  = element_blank(),
-                            panel.background = element_rect(fill = "white", colour = "black") , 
-                            panel.grid.major.y = element_line(color = "gray90"),
-                            text = element_text(size= 20),
-                            plot.title = element_text(vjust=0, colour = "#0B6357",face = "bold", size = 40))
-  
   
   # initialize base data mapping for plot
-  plot_1 <- ggplot(data = mean_cv, aes(x = k, y = cv_means))
+  plot_2_5_b <- ggplot(data = mean_cv, aes(x = k, y = cv_means))
   
-  plot_1 <- plot_1 + geom_point(size = 1) + geom_line() + plot_attributes
-  plot_1
+  plot_2_5_b <- plot_2_5_b + geom_point(size = 1) + geom_line() + plot_attributes
+  plot_2_5_b
   
   #=================#
   # ==== part c ====
@@ -468,7 +453,7 @@ h_opt <- (15/(v2k*1000))^(1/5)
      se_mat <- do.call(cbind, se_list)
       
      # sum the rows 
-      betas <- rowMeans(beta_mat)
+      betas <- rowMeans(b_mat)
       se <-   rowMeans(se_mat)
       
       # now write a function to plot the u hat funciton 
@@ -486,100 +471,229 @@ h_opt <- (15/(v2k*1000))^(1/5)
       }
       
       # plot the true functin 
-      p_c_plot <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
-      p_c_plot <- p_c_plot + stat_function(fun = true_fun, 
+      plot_2_5_c <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
+      plot_2_5_c <- plot_2_5_c + stat_function(fun = true_fun, 
                                            color = "blue") 
-      p_c_plot <- p_c_plot + plot_attributes + xlim(-1,1) 
+      plot_2_5_c <- plot_2_5_c + plot_attributes + xlim(-1,1) 
       
-      p_c_plot
       
-      # now plot that function 
-      # plot the true functin 
-      p_c_plot <- p_c_plot + stat_function(fun = u_hat_fun, 
-                                             color = "red") 
+      # now add u hat function
+      plot_2_5_c <- plot_2_5_c + stat_function(fun = u_hat_fun, 
+                                             color = "red", linetype = 2) 
       
-      p_c_plot
-      
+      plot_2_5_c <- plot_2_5_c + scale_colour_identity("Function", guide="legend", 
+                                                labels = c("U hat", "True U"), 
+                                                breaks = c("red", "blue")) + theme(axis.title.y=element_blank())
       # create some data to plot with the standard errors 
       plot_data <- data.table(x = seq(-1,1,.2))
       plot_data[, y_hat := u_hat_fun(x)]
       plot_data[, se := se]
       
-      p_c_plot <- p_c_plot + geom_point(data = plot_data, mapping = aes(x = x, y = y_hat), 
+      plot_2_5_c <- plot_2_5_c + geom_point(data = plot_data, mapping = aes(x = x, y = y_hat), 
                                            color = "red") 
     
-      p_c_plot <- p_c_plot + geom_errorbar(data = plot_data, aes(ymin=y_hat-se, ymax=y_hat+se), width=.1) 
+      plot_2_5_c <- plot_2_5_c + geom_errorbar(data = plot_data, aes(ymin=y_hat-se, ymax=y_hat+se), width=.1) 
       
-      p_c_plot
-  
-  #===============================================================#
-  # ==== dont need this anymore caues I can do it in paralell ====
-  #===============================================================#
-  
-  #   # start a list for results 
-  #   results_list <- vector("list", length = M)
-  # # now to M simulations 
-  #   for(i in 1:M){
-  #     
-  #     # generate data 
-  #     # start data.table for random data, take a random draw for weighted normals 
-  #     r_dt <- data.table( r1 = sample(1:2,prob=c(.5,.5),size=n,replace=T) )
-  #     
-  #     # draw a random number from appropriate normal dist according to r1
-  #     r_dt[r1 == 1, rdraw := rnorm(.N,-1.5,1.5)]
-  #     r_dt[r1 == 2, rdraw := rnorm(.N,1,1)]
-  #     r_dt[, r1 := NULL]
-  #     
-  #     
-  #     # get IMSE 
-  #     results_i <- imse_f(in_data = r_dt, x_v = "rdraw")
-  #     results_i[, sim := i]
-  #     results_list[[i]] <- results_i
-  #     
-  #   }
-  # 
-  #   # bind results 
-  #   results_dt <- rbindlist(results_list)
-  
-  
-  
-  #=============================#
-  # ==== Sanity check plots ====
-  #=============================#
+      # print it out to see if it looks alright 
+      plot_2_5_c
+      
+      
+  #=====================#
+  # ==== save plots ====
+  #=====================#
+      
+      # only save data if this isn't a test run 
+      if(!opt_test_run){
 
-  
-  # plot function
-  plot <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
-  plot <- plot + stat_function(fun = phi_2, 
-                                           args = list(mean = mean_i, v = var_i), 
-                                           color = "blue") 
-  plot <- plot + xlim(-10,10)
-  plot
-  
-  
-  
-  plot <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
-  plot <- plot + stat_function(fun = f_int, 
-                               color = "blue") 
-  plot <- plot + xlim(-10,10)
-  plot
-  
-  
-  
-  
-  
-  plot <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
-  plot <- plot + stat_function(fun = phi_2, 
-                               args = list(mean = -1.5, v = 1.5), 
-                               color = "blue") 
-  plot <- plot + xlim(-10,10)
-  plot
-  
-  
-  
-  
-  
-  f_int
-  power_plot <- power_plot + xlim(-5000,5000) + xlab("tau") + ylab("Power")  + plot_attributes
-  power_plot
+        # save the plot 
+        png("c:/Users/Nmath_000/Documents/Code/courses/econ 675/PS_2_tex/plot_2_5_b.png", height = 800, width = 800, type = "cairo")
+        print(plot_2_5_b)
+        dev.off()
+        
+        # save the plot 
+        png("c:/Users/Nmath_000/Documents/Code/courses/econ 675/PS_2_tex/plot_2_5_c.png", height = 800, width = 800, type = "cairo")
+        print(plot_2_5_c)
+        dev.off()
+        
+        
+      }
+
+      
+      
+#=====================#
+# ==== question 3 ====
+#=====================#
+
+  #=================#
+  # ==== Part a ====
+  #=================#
+    
+    d   = 5
+    theta_n = 1
+    
+    
+    data_gen <- function(n) {
+      X <- matrix(runif(n*d,-1,1), n, d)
+      V <- rnorm(n)
+      x.norm = sapply(1:n,function(i) t(x[i,])%*%x[i,])
+      E      = 0.3637899*(1+x.norm)*v
+      g0.x   =exp(x.norm)
+      
+      U <- rnorm(n)
+      tt <- as.numeric((sqrt(x.norm)+u)>1)
+      Y <- tt + g0.x + e
+      return(list(Y=Y, X=X, tt=tt))
+    } 
+    
+    
+    # generate the polynomial basis
+    gen.P = function(Z,K) {
+      if (K==0)   out = NULL;
+      if (K==1)   out = poly(Z,degree=1,raw=TRUE);
+      if (K==2)  {out = poly(Z,degree=1,raw=TRUE); for (j in 1:ncol(Z)) out = cbind(out,Z[,j]^2);}
+      if (K==2.5) out = poly(Z,degree=2,raw=TRUE);
+      if (K==3)  {out = poly(Z,degree=2,raw=TRUE); for (j in 1:ncol(Z)) out = cbind(out,Z[,j]^3);}
+      if (K==3.5) out = poly(Z,degree=3,raw=TRUE);
+      if (K==4)  {out = poly(Z,degree=3,raw=TRUE); for (j in 1:ncol(Z)) out = cbind(out,Z[,j]^4);}
+      if (K==4.5) out = poly(Z,degree=4,raw=TRUE);
+      if (K==5)  {out = poly(Z,degree=4,raw=TRUE); for (j in 1:ncol(Z)) out = cbind(out,Z[,j]^5);}
+      if (K==5.5) out = poly(Z,degree=5,raw=TRUE);
+      if (K>=6)  {out = poly(Z,degree=5,raw=TRUE); for (k in 6:K) for (j in 1:ncol(Z)) out = cbind(out,Z[,j]^k);}
+      ## RETURN POLYNOMIAL BASIS
+      return(out)
+    }
+
+  #=================#
+  # ==== part b ====
+  #=================#
+    
+    n   <- 500
+    K   <- c(1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8, 9, 10)
+    K.r <- c(6, 11, 21, 26, 56, 61, 126, 131, 252, 257, 262, 267, 272, 277)
+    nK  <- length(K)
+    M <- ifelse(opt_test_run, 10, 1000)
+    theta.hat <- matrix(NaN, ncol=nK, nrow=M)
+    se.hat    <- theta.hat
+    
+    set.seed(123)
+    ptm <- proc.time()
+    for (m in 1:M) {
+      data <- data_gen(n)
+      X <- data$X
+      Y <- data$Y
+      tt <- data$tt
+      for (k in 1:nK) {
+        X.pol <- cbind(1, gen.P(X, K[k]))
+        X.Q   <- qr.Q(qr(X.pol))
+        MP     <- diag(rep(1,n)) - X.Q %*% t(X.Q)
+        Y.M <- MP %*% Y
+        tt.M <- MP %*% tt
+        theta.hat[m, k] <- (t(tt.M) %*% Y.M) / (t(tt.M) %*% tt.M)
+        Sigma <- diag((as.numeric((Y.M - tt.M*theta.hat[m, k])))^2)
+        se.hat[m, k] <- sqrt(t(tt.M) %*% Sigma %*% tt.M) / (t(tt.M) %*% tt.M)
+      }
+    }
+    proc.time() - ptm
+    
+    table <- matrix(NaN, ncol=6, nrow=nK)
+    for (k in 1:nK) {
+      table[k, 1] <- K.r[k]
+      table[k, 2] <- mean(theta.hat[, k]) - 1                           # bias
+      table[k, 3] <- sd(theta.hat[, k])                                 # standard deviation
+      table[k, 4] <- table[k, 2]^2 + table[k, 3]^2                      # mse
+      table[k, 5] <- mean(se.hat[, k])                                  # mean standard error
+      table[k, 6] <- mean((theta.hat[, k] - 1.96 * se.hat[, k] > 1) | 
+                            (theta.hat[, k] + 1.96 * se.hat[, k] < 1))  # rejection rate
+    }
+
+    table <- data.table(table)
+    setnames(table, colnames(table), c("K", "Theta", "Bias", "S.D", "V_HCO", "Rejection rate"))
+    
+#=====================#
+# ==== save table ====
+#=====================#
+    
+    # save IMSE by h results 
+    print(xtable(table, type = "latex", 
+                 digits = 3), 
+          file = "C:/Users/Nmath_000/Documents/Code/courses/econ 675/PS_2_tex/Q3_4_b.tex",
+          include.rownames = FALSE,
+          floating = FALSE)
+    
+    
+#=====================#
+# ==== Q3. 4. (c) ====
+#=====================#
+    
+
+    
+    # cross validation function
+    K.CV <- function(tt, X, Y) {
+      temp <- rep(NaN, nK)
+      for (k in 1:nK) {
+        X.pol <- cbind(1, tt, gen.P(X, K[k]))
+        X.Q   <- qr.Q(qr(X.pol))
+        XX <- X.Q %*% t(X.Q)
+        Y.hat <- XX %*% Y
+        W <- diag(XX)
+        temp[k] <- mean(((Y-Y.hat) / (1-W))^2)
+      }
+      return(which.min(temp))
+    }
+    
+    theta.hat2 <- rep(NaN, M)
+    se.hat2    <- theta.hat2
+    K.hat2     <- theta.hat2
+    
+    set.seed(123)
+    ptm <- proc.time()
+    for (m in 1:M) {
+      data <- data_gen(n)
+      X <- data$X; Y <- data$Y; tt <- data$tt
+      k.opt <- K.CV(tt, X, Y)
+      X.pol <- cbind(1, gen.P(X, K[k.opt]))
+      X.Q   <- qr.Q(qr(X.pol))
+      MP    <- diag(rep(1,n)) - X.Q %*% t(X.Q)
+      Y.M   <- MP %*% Y
+      tt.M  <- MP %*% tt
+      theta.hat2[m] <- (t(tt.M) %*% Y.M) / (t(tt.M) %*% tt.M)
+      Sigma         <- diag((as.numeric((Y.M - tt.M*theta.hat[m, k])))^2)
+      se.hat2[m]    <- sqrt(t(tt.M) %*% Sigma %*% tt.M) / (t(tt.M) %*% tt.M)
+      K.hat2[m]     <- K.r[k.opt]
+    }
+    proc.time() - ptm
+    
+    # summary of the cross validation 
+    table(K.hat2)
+    
+    # estimator
+    summary(theta.hat2)
+    sd(theta.hat2)
+    summary(se.hat2)
+    sd(se.hat2)
+    
+    par(mfrow=c(1,2))
+    hist(theta.hat2, freq=FALSE, xlab="theta-hat", ylab="", main="")
+    lines(c(mean(theta.hat2), mean(theta.hat2)), c(-1, 20), col="red", lwd=3)
+    hist(se.hat2, freq=FALSE, xlab="s.e.", ylab="", main="")
+    lines(c(mean(se.hat2), mean(se.hat2)), c(-1, 80), col="red", lwd=3)
+    
+    par(mfrow=c(1,2))
+    CI.l <- theta.hat2 - 1.96 * se.hat2
+    CI.r <- theta.hat2 + 1.96 * se.hat2
+    # rejection rate
+    mean(1 < CI.l | 1 > CI.r)
+    plot(1:M, CI.l, type="l", ylim=c(0,2), xlab="simulations", ylab="CI")
+    lines(1:M, CI.r)
+    abline(1, 0, col="red", lwd=2)
+    
+    temp <- sort(CI.l, index.return=TRUE)
+    CI.l <- temp$x
+    CI.r <- CI.r[temp$ix]
+    plot(1:M, CI.l, type="l", ylim=c(0,2), xlab="simulations", ylab="CI")
+    lines(1:M, CI.r)
+    abline(1, 0, col="red", lwd=2)    
+    
+    
+
   
