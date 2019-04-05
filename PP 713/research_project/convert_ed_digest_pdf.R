207c69e8924550bc503713556ebf934d287cdad1
Nathan-Mather <njmather@umich.edu>
2019-04-05 13:43:58 -0400

    add some more comments in case someone tries to use this

--- PP 713/research_project/convert_ed_digest_pdf.R
+++ PP 713/research_project/convert_ed_digest_pdf.R
@@ -14,22 +14,23 @@ library(xlsx)
 library(tabulizer)
 library(data.table)
 
-# have you done the shiny app?
+# have you done the shiny app? (start witth this as false)
 shiny_app_done <- FALSE
 
-# get path base 
+# get path base, path to your pdf 
 path_pdf <- "C:/Users/Nmath_000/Documents/MI_school/Second Year/PP 713/research_project/digest_of_ed_2016.pdf"
 
 #=======================#
 # ==== convert file ====
 #=======================#
 
-
+# if you havnt done this yet, do it 
 if(!shiny_app_done){
-  # find areas with the table on it ()
+  
+  # find areas with the table on it (). This opens of a shiny app of the pdf and you just have to highlight the areas with the table you want 
   area <- locate_areas(path_pdf, pages = 420, resolution = 70L)
   
-  # save this so I dont have to do the shiny app again 
+  # save this so I dont have to do the shiny app again. Just incase something crashes or whatever 
   save(area, file = "C:/Users/Nmath_000/Documents/MI_school/Second Year/PP 713/research_project/pdf_area.R")
 
   
@@ -40,9 +41,14 @@ if(!shiny_app_done){
 }
 
 
-  # convert page we need 
+  # convert page we need. Stream worked better for me with this file but sometimes "lattice" is better \
+  #  i am telling it not to guess where the table is but to use the area we highlighted in the shiny app above 
   out1 <- extract_tables(path_pdf, method = "stream", pages = 420, guess = FALSE, area = area )
-  out1_dt <- data.table(out1[[1]][-c(1,2),])
+ 
+  # grab data and skip over column names 
+   out1_dt <- data.table(out1[[1]][-c(1,2),])
+   
+  # now use the column names do set column names in my data 
   setnames(out1_dt, colnames(out1_dt), make.names(out1[[1]][1,]))
 
   #=======================#
@@ -57,15 +63,16 @@ if(!shiny_app_done){
   
   # extract the higher age
   # this is first remove the number before "to" 
+  # this is a function that is going to be helpful. It splits a string and grabs out the part of the partition you want 
   variable_splitter <- function (in_var_split = NULL, in_val_position = NULL, in_val_delim = NULL){
     out_var <- sapply(strsplit(in_var_split, in_val_delim), function(x) x[in_val_position])
     return(out_var)
   }
   
-  # get columns that need fixing 
+  # get columns that need fixing. In my case all the years contain x so it's a fast way to grab them 
   cols_to_fix <- grep("X", colnames(out1_dt), value = TRUE)
   
-  # strip out the "x to " portion of the string 
+  # strip out the "x to " portion of the string for all the columns. 
   out1_dt[ , (cols_to_fix) := lapply(.SD, variable_splitter, in_val_position= 2, in_val_delim=" to "), .SDcols = cols_to_fix]
   
   # now strip out the footnores that made it in 
